014c5e846b61cf86349cd8c8769b37479c2771fa
Chibuikem Amaechi <chibuikem_amaechi@Chibuikems-MacBook-Pro.local>
2017-12-08 11:25:19 -0600

    Modified .main.cpp.swp and main.cpp

--- main.cpp
+++ main.cpp
@@ -27,52 +27,52 @@ const int NUMBER_OF_PEOPLE = 50;
 int main()
 {
     srand(time(NULL));
-	int p[NUMBER_OF_PEOPLE];
+    int p[NUMBER_OF_PEOPLE];
 
-	output(p, NUMBER_OF_PEOPLE);
+    output(p, NUMBER_OF_PEOPLE);
 
-	return 0;
+    return 0;
 }
 
 double birthdayProbability(int people[], int numberOfPeople)
 {
-	int count = 0;
-	int stop = 0;
+    int count = 0;
+    int stop = 0;
 
-	for (int i =  1; i <= 5000; i++)
-	{
-		for (int j = 0; j < numberOfPeople; j++)
-			people[j] = rand() % 365 + 1;
+    for (int i =  1; i <= 5000; i++)
+    {
+        for (int j = 0; j < numberOfPeople; j++)
+            people[j] = rand() % 365 + 1;
 
-		for (int k = 0; stop != -1 && k < numberOfPeople; k++)
-			findIdenticalBirthDates(people, numberOfPeople, k, count, stop);
+        for (int k = 0; stop != -1 && k < numberOfPeople; k++)
+            findIdenticalBirthDates(people, numberOfPeople, k, count, stop);
 
-		stop = 0;
-	}
+        stop = 0;
+    }
 
-	return count/5000.0;
+    return count/5000.0;
 }
 
 void findIdenticalBirthDates(int people[], int numberOfPeople, int targetIndex, int& count, int& stop)
 {
-	for (int i = numberOfPeople - 1; i > targetIndex; i--)
-	{
-		if (people[i] == people[targetIndex])
-		{
-			stop = -1;
-			++count;
-			break;
-		}
-	}
+    for (int i = numberOfPeople - 1; i > targetIndex; i--)
+    {
+        if (people[i] == people[targetIndex])
+        {
+            stop = -1;
+            ++count;
+            break;
+        }
+    }
 }
 
 void output(int people[], int size)
 {
-	cout.setf(ios::showpoint);
-	cout.setf(ios::fixed);
-	cout.precision(4);
+    cout.setf(ios::showpoint);
+    cout.setf(ios::fixed);
+    cout.precision(4);
 
-	for (int i = 2; i <= size; i++)
-		cout<<"For "<<i<<" people, the probability of two birthdays is, "<<birthdayProbability(people, i)<<"\n\n";
+    for (int i = 2; i <= size; i++)
+        cout<<"For "<<i<<" people, the probability of two birthdays is, "<<birthdayProbability(people, i)<<"\n\n";
 }
 
